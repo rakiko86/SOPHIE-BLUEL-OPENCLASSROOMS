886b08df8dfaef20afd662891232cc5c47aaad95
Rekia Harrat <rekia.harrat86@gmail.com>
2024-07-13 13:26:57 +0200

    feat: Delete modal works

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,55 +1,67 @@
+// Sélection des éléments du DOM
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
+const galerieModal = document.querySelector(".galerieModal");
+const apiUrl = 'http://localhost:5678/api';
+let token = window.localStorage.getItem("authToken");
 
-modalTriggers.forEach(trigger => trigger.addEventListener('click', toggleModal))
- function toggleModal(){
-modalContainer.classList.toggle("active")
- }
+// Gestion des événements pour les triggers du modal
+modalTriggers.forEach(trigger => trigger.addEventListener('click', toggleModal));
 
- const galeryModal = document.querySelector(".galeryModal");
- async function displayGalleryModal(){
-    galeryModal.innerHTML ="";
-    const galery = await getWorks();
-    galery.forEach(work => {
+function toggleModal() {
+    modalContainer.classList.toggle("active");
+}
+
+// Affichage du modal de la galerie
+async function displayGalerieModal() {
+    galerieModal.innerHTML = "";
+    const projects = await fetchWorks();
+    projects.forEach(project => {
         const figure = document.createElement('figure');
         const img = document.createElement('img');
         const span = document.createElement('span');
-        const trash =document.createElement ('i');
-        trash.classList.add('fa-solid','fa-trash-can');
-        trash.id = work.id;
-        img.src = work.imageUrl;
+        const trash = document.createElement('i');
+        img.src = project.imageUrl;
+        img.alt = project.title;
+
+        trash.classList.add('fa-solid', 'fa-trash-can');
+        trash.id = project.id;
+       
         span.appendChild(trash);
         figure.appendChild(span);
         figure.appendChild(img);
-        galleryModal.appendChild(figure);
+        galerieModal.appendChild(figure);
     });
 
-    deleteWorks()
+    DeleteWorks();
+}
 
- }
-
- displayGaleryModal()
-  function deleteWorks(){
+// Attachement des événements de suppression
+function attachDeleteEvent() {
     const trashAll = document.querySelectorAll('.fa-trash-can');
     trashAll.forEach(trash => {
-        trash.addEventListener("click",()=>{
-            const id =trash.id;
+        trash.addEventListener("click", async (e) => {
+            const id = trash.id;
             const init = {
-                method:"DELETE",
-                headers:{ Authorization: "Bearer " + authToken},
-            }
-            fetch('http://localhost:5678/api/works/'+id,init)
-            .then((response)=> {
-                if(!response.ok){
-                    console.log("Le delete n'est pas éffectué!")
+                method: "DELETE",
+                headers: { Authorization: "Bearer " + token },
+            };
+
+            try {
+                const response = await fetch(`${apiUrl}/works/${id}`, init);
+                if (!response.ok) {
+                    console.log("Le delete n'est pas effectué!");
+                    return;
                 }
-                return response.json
-            })
-            .then((data)=>{
-                console.log("Le delete est effectué voici la data:",data)
-                displayGalleryModal()
-                displayWorks()
-            })
-        })
-    })
-  }
+                console.log("Le delete est effectué!");
+                await displayGalerieModal();
+                const projects = await fetchWorks();
+                displayWorks(projects);
+            } catch (error) {
+                console.error('Erreur lors de la suppression du travail:', error);
+            }
+        });
+    });
+}
+// Appel de l'affichage du modal de la galerie
+displayGalerieModal();
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,137 +1,142 @@
 
-const apiUrl = 'http://localhost:5678/api';
-let token = window.localStorage.getItem("authToken");
 
-console.log("Tokne is here:" + token)
 
+// Fetch des travaux depuis l'API
 async function fetchWorks() {
-  try {
-    const response = await fetch(`${apiUrl}/works`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    try {
+        const response = await fetch(`http://localhost:5678/api/works`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Erreur lors de la récupération des travaux:', error);
+        return [];
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Erreur lors de la récupération des travaux:', error);
-    return [];
-  }
 }
 
+// Affichage des travaux
 function displayWorks(works) {
-  const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = ''; // Clear existing content
-  works.forEach((work) => {
-    const figure = document.createElement('figure');
-    const img = document.createElement('img');
-    img.src = work.imageUrl;
-    img.alt = work.title;
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = ''; // Clear existing content
+    works.forEach(work => {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        img.src = work.imageUrl;
+        img.alt = work.title;
 
-    const figcaption = document.createElement('figcaption');
-    figcaption.innerText = work.title;
+        const figcaption = document.createElement('figcaption');
+        figcaption.innerText = work.title;
 
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
-  });
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
 }
 
+// Trier par catégorie
 function sortByCategory(data) {
-  return data.reduce((acc, item) => {
-    const categoryName = item.category.name;
-    if (!acc[categoryName]) {
-      acc[categoryName] = [];
-    }
-    acc[categoryName].push(item);
-    return acc;
-  }, {});
+    return data.reduce((acc, item) => {
+        const categoryName = item.category.name;
+        if (!acc[categoryName]) {
+            acc[categoryName] = [];
+        }
+        acc[categoryName].push(item);
+        return acc;
+    }, {});
 }
 
+// Fetch des catégories depuis l'API
 async function fetchCategories() {
-  try {
-    const response = await fetch(`${apiUrl}/categories`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    try {
+        const response = await fetch(`http://localhost:5678/api/categories`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Erreur lors de la récupération des catégories:', error);
+        return [];
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Erreur lors de la récupération des catégories:', error);
-    return [];
-  }
 }
 
+// Générer le menu des catégories
 function generateCategoriesMenu(categories, projects) {
-  const categoriesMenu = document.getElementById('categories-menu');
-  categoriesMenu.innerHTML = '';
-  const button = document.createElement('button');
-  button.textContent = "Tous";
-  categoriesMenu.appendChild(button);
-
-  categories.forEach(category => {
+    const categoriesMenu = document.getElementById('categories-menu');
+    categoriesMenu.innerHTML = '';
     const button = document.createElement('button');
-    button.textContent = category.name;
-    button.addEventListener('click', () => {
-      filterProjects(category.name, projects);
-      setActiveCategory(button);
-    });
+    button.textContent = "Tous";
     categoriesMenu.appendChild(button);
-  });
-  const firstButton = categoriesMenu.querySelector('button');
-  firstButton.classList.add('active');
+
+    categories.forEach(category => {
+        const button = document.createElement('button');
+        button.textContent = category.name;
+        button.addEventListener('click', () => {
+            filterProjects(category.name, projects);
+            setActiveCategory(button);
+        });
+        categoriesMenu.appendChild(button);
+    });
+    const firstButton = categoriesMenu.querySelector('button');
+    firstButton.classList.add('active');
 }
 
+// Filtrer les projets par catégorie
 function filterProjects(category, projects) {
-  if (category === 'Tous') {
-    displayWorks(projects);
-  } else {
-    const filteredProjects = projects.filter(project => project.category.name === category);
-    displayWorks(filteredProjects);
-  }
+    if (category === 'Tous') {
+        displayWorks(projects);
+    } else {
+        const filteredProjects = projects.filter(project => project.category.name === category);
+        displayWorks(filteredProjects);
+    }
 }
 
+// Définir la catégorie active
 function setActiveCategory(activeButton) {
-  const buttons = document.querySelectorAll('#categories-menu button');
-  buttons.forEach(button => {
-    button.classList.remove('active');
-  });
-  activeButton.classList.add('active');
+    const buttons = document.querySelectorAll('#categories-menu button');
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+    activeButton.classList.add('active');
 }
 
+// Initialisation de l'application
 document.addEventListener("DOMContentLoaded", () => {
-  initializeApp();
+    initializeApp();
 });
 
 async function initializeApp() {
-  try {
-    const [projects, categories] = await Promise.all([fetchWorks(), fetchCategories()]);
-    displayWorks(projects);
-    if (isUserLoggedIn()) {
-      enableAdminMode();
-    } else {
-      generateCategoriesMenu(categories, projects);
+    try {
+        const [projects, categories] = await Promise.all([fetchWorks(), fetchCategories()]);
+        displayWorks(projects);
+        if (isUserLoggedIn()) {
+            enableAdminMode();
+        } else {
+            generateCategoriesMenu(categories, projects);
+        }
+    } catch (error) {
+        console.error("Error initializing app:", error);
     }
-  } catch (error) {
-    console.error("Error initializing app:", error);
-  }
 }
 
+// Vérifier si l'utilisateur est connecté
 function isUserLoggedIn() {
-  return token !== null; // Assuming 'token' is globally defined
+    return token !== null; // Assuming 'token' is globally defined
 }
 
+// Activer le mode administrateur
 function enableAdminMode() {
-  document.querySelector(".login").innerText = "Logout";
-  
- 
+    document.querySelector(".login").innerText = "Logout";
 }
+
+
